4281c2302ecc48b63f3794cf2441c2dc5593881e
Eirik Horvath <eirik@geta.no>
2017-09-25 14:45:43 +0200

    Excercise C2

---
+++ CommerceFundamentalsWeb/Controllers/Catalog/CatalogControllerBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.SessionState;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Mvc;
+using EPiServer.Web.Routing;
+
+namespace CommerceFundamentalsWeb.Controllers.Catalog
+{
+    [SessionState(SessionStateBehavior.Disabled)]
+    public class CatalogControllerBase<T> : ContentController<T> where T : CatalogContentBase
+    {
+        public T CurrentContent
+        {
+            get
+            {
+                var pageRouteHelper = ServiceLocator.Current.GetInstance<IPageRouteHelper>();
+                return pageRouteHelper.Page as T;
+            }
+        }
+    }
+}
---
+++ CommerceFundamentalsWeb/Controllers/Catalog/ShirtVariationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommerceFundamentalsWeb.Models.Catalog;
+
+namespace CommerceFundamentalsWeb.Controllers.Catalog
+{
+    public class ShirtVariationController : CatalogControllerBase<ShirtVariation>
+    {
+        // GET: FashionNode
+        public ActionResult Index()
+        {
+            return View(CurrentContent);
+        }
+    }
+}
