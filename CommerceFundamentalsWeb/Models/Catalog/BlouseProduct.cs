e39e9b07ef19510563f2a5761098ae5a4965ff66
Eirik Horvath <eirik@geta.no>
2017-09-26 09:27:58 +0200

    set contentType

--- CommerceFundamentalsWeb/Models/Catalog/BlouseProduct.cs
+++ CommerceFundamentalsWeb/Models/Catalog/BlouseProduct.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Catalog.DataAnnotations;
 
 namespace CommerceFundamentalsWeb.Models.Catalog
 {
+    [CatalogContentType(
+        DisplayName = "Blouse Product",
+        GUID = "BD70D68E-354B-4A69-AFB2-2AF56F5B993C",
+        MetaClassName = "Blouse_Product")]
     public class BlouseProduct : ProductContent
     {
 
