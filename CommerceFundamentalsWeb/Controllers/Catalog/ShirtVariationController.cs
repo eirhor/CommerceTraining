d37313b7f2cb19f2c13f179e2f9c358c0141cb28
Eirik Horvath <eirik@geta.no>
2017-09-27 12:52:06 +0200

    Add random price service and initialize as default price processor.

--- CommerceFundamentalsWeb/Controllers/Catalog/ShirtVariationController.cs
+++ CommerceFundamentalsWeb/Controllers/Catalog/ShirtVariationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CommerceFundamentalsWeb.Business;
 using CommerceFundamentalsWeb.Models.Catalog;
 using CommerceFundamentalsWeb.Models.Pages;
 using CommerceFundamentalsWeb.Models.ViewModels;
--- CommerceFundamentalsWeb/Infrastructure/EPiServerCommerceInitializationModule.cs
+++ CommerceFundamentalsWeb/Infrastructure/EPiServerCommerceInitializationModule.cs
@@ -1,9 +1,11 @@
 using System.Web.Mvc;
 using System.Web.Routing;
+using CommerceFundamentalsWeb.Business;
 using EPiServer.Commerce.Routing;
 using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
 using EPiServer.ServiceLocation;
+using Mediachase.Commerce.Pricing;
 
 namespace CommerceFundamentalsWeb.Infrastructure
 {
@@ -25,6 +27,9 @@ namespace CommerceFundamentalsWeb.Infrastructure
         public void ConfigureContainer(ServiceConfigurationContext context)
         {
             DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.StructureMap()));
+
+            context.StructureMap().Configure(x => x.For<IPriceService>().Use<RandomPriceService>());
+
         }
     }
 }
